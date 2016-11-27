76f1337f2ab9d64a8dc03998f38228c5af561240
Linus Lundin <linus.lundin@gmail.com>
2016-11-27 20:58:05 +0000

    Started work on rrd support

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Node.js implementation to port existing atom1.5 scripts to communicate with mqtt
 ```bash
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt install librrd-dev
 ```
 
 ## Setup
--- uscript/skohylla.js
+++ uscript/skohylla.js
@@ -19,9 +19,9 @@ class skohylla {
         this.last_dimmer_value = 0;
         const SkoMovementTimeout = 100;
         
-        mqttManager.registerCallback(this,"CAN/"+this.mySensor+"/Status",this.sensorOnMessage);
-        mqttManager.registerCallback(this,"CAN/"+this.myDimmer+"/Pwm",this.dimmerOnMessage);
-        
+        mqttManager.registerCallback(`CAN/${this.mySensor}/Status`, this.sensorOnMessage.bind(this));
+        mqttManager.registerCallback(`CAN/${this.myDimmer}/Pwm`, this.dimmerOnMessage.bind(this));
+
         /*
         Module_RegisterToOnMessage(aliasnameSensor, function(alias_name, command, variables) { self.sensorOnMessage(alias_name, command, variables) });
         Module_RegisterToOnChange( aliasnameSensor, function(alias_name, available,test) { self.sensorOnline(alias_name, available,test) });
@@ -30,26 +30,26 @@ class skohylla {
         /* Start interval timer for sending timestamp to network. Arguments are the callback function and time in milliseconds 
         used to make sure an eth-node gets its init packet (600s) */
         //this.myTimer = Timer_SetTimer(function(timer) {self.timerUpdate(timer)}, 5000, true);
-        setInterval(this.timerUpdate, 5000);
+        setInterval(this.timerUpdate.bind(this), 5000);
         console.log("Skohylla created.", this.mySensor, this.myDimmer, this.triggstate);
     }
     
-    sensorOnMessage(obj,topic, value)
+    sensorOnMessage(topic, value)
     {
       console.log("Got Callback!!!!");
       //Log("\033[33mMessage.\033[0m\n");
-      console.log("Is ", value, "===" , obj.triggstate);
-      if (value === obj.triggstate) {
-        console.log("Match triggerstate! ",obj.last_dimmer_value, "=", "0");
+      console.log("Is ", value, "===" , this.triggstate);
+      if (value === this.triggstate) {
+        console.log("Match triggerstate! ",this.last_dimmer_value, "=", "0");
         //Log("\033[33mPin low.\033[0m\n");
-        if (obj.last_dimmer_value == "0") {
+        if (this.last_dimmer_value == "0") {
           //log(this.myName + ":" + this.myId + "> Light on.\n");
-          if (obj.oldPwmValue == 0) {
-            obj.oldPwmValue = obj.defaultValue;
+          if (this.oldPwmValue == 0) {
+            this.oldPwmValue = this.defaultValue;
           }
-          console.log("Send new value to dimmer: ", obj.oldPwmValue);
+          console.log("Send new value to dimmer: ", this.oldPwmValue);
           //Dimmer_AbsoluteFade(this.myDimmer, 129, this.oldPwmValue);
-          obj.mqttManager.publish("CAN/"+obj.myDimmer, '{"command":"Abs_Fade","variables":{"EndValue":'+obj.oldPwmValue+',"Speed":135}}');
+          this.mqttManager.publish("CAN/"+this.myDimmer, '{"command":"Abs_Fade","variables":{"EndValue":'+this.oldPwmValue+',"Speed":135}}');
           //CAN/koksbank -m '{"command":"Abs_Fade","variables":{"EndValue":8000,"Speed":135}}'
           
         }
@@ -57,14 +57,14 @@ class skohylla {
         //Log("\033[33mPin high.\033[0m\n");
         console.log("Not Match triggerstate!");
       }
-      if (obj.turnOffCnt <  obj.SkoMovementTimeout) {
-        obj.turnOffCnt = obj.SkoMovementTimeout;
+      if (this.turnOffCnt <  this.SkoMovementTimeout) {
+        this.turnOffCnt = this.SkoMovementTimeout;
       }
     }
     
-    dimmerOnMessage(obj,topic, value)
+    dimmerOnMessage(topic, value)
     {
-      obj.last_dimmer_value = value;
+      this.last_dimmer_value = value;
       console.log("Got dimmer Callback!!!! ", value);
     }
     
